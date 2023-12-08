aff4478680c67b74f1a8b3765f7e7ff50615c986
12manel123 <manelcastellvicerezuela@gmail.com>
2023-12-08 10:27:40 +0100

    Component Error Fix

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
 })
